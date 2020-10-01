eff0f899cde814c300d3d45b3cdf04222d1389a1
Afeef Rabbani <afeef.rabbani@gmail.com>
2020-10-01 13:36:34 +0530

    updated portfolio

---
+++ dist/assets/js/function.js
@@ -0,0 +1,8 @@
+const getRandomWord = async() => {
+    const res = await fetch("/getword");
+    const word = await res.text();
+
+    document.getElementById("random-word").innerHTML = word;
+}
+
+getRandomWord();
