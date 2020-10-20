4bec6c7d58c2a46a03a54f299e38e3bada6a7a57
Afeef Rabbani <afeef.rabbani@gmail.com>
2020-10-20 14:47:59 +0530

    redirects

--- dist/assets/js/function.js
+++ dist/assets/js/function.js
@@ -1,8 +1,8 @@
-const getRandomWord = async() => {
-    const res = await fetch("/getword");
-    const word = await res.text();
+// const getRandomWord = async() => {
+//     const res = await fetch("/getword");
+//     const word = await res.text();
 
-    document.getElementById("random-word").innerHTML = word;
-}
+//     document.getElementById("random-word").innerHTML = word;
+// }
 
-getRandomWord();
+// getRandomWord();
